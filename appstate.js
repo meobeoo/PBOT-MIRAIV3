1498f475b34e34317f4941a3e6de313133499461
meobeoo <tranthanhnhat24@gmail.com>
2026-06-14 23:31:05 +0700

    Update appstate.js

--- appstate.js
+++ appstate.js
@@ -1,1 +1,74 @@
-
+[
+  {
+    "key": "fr",
+    "value": "0KhDEzBdnLR4wJToq.AWdKD0qCmOj1Hghux9-C4BvuH5MSoKY_GANLJ6oeb-SY0zmLS1w.BqKqE8..AAA.0.0.BqLtc5.AWdCKjVXbsRApbUMgV0LlQFb--A",
+    "domain": "facebook.com",
+    "path": "/",
+    "secure": true,
+    "httpOnly": false
+  },
+  {
+    "key": "datr",
+    "value": "Z7dbabCKBd7CjJi5sz2v4ANa",
+    "domain": "facebook.com",
+    "path": "/",
+    "secure": true,
+    "httpOnly": false
+  },
+  {
+    "key": "pas",
+    "value": "61585748627785%3AMz5GJ1CBei%2C61590536292093%3AJvNt5FCn7q",
+    "domain": "facebook.com",
+    "path": "/",
+    "secure": true,
+    "httpOnly": false
+  },
+  {
+    "key": "ps_n",
+    "value": "1",
+    "domain": "facebook.com",
+    "path": "/",
+    "secure": true,
+    "httpOnly": false
+  },
+  {
+    "key": "xs",
+    "value": "43%3Abft0nh5FpEZGFw%3A2%3A1781442900%3A-1%3A-1",
+    "domain": "facebook.com",
+    "path": "/",
+    "secure": true,
+    "httpOnly": false
+  },
+  {
+    "key": "c_user",
+    "value": "61590536292093",
+    "domain": "facebook.com",
+    "path": "/",
+    "secure": true,
+    "httpOnly": false
+  },
+  {
+    "key": "locale",
+    "value": "vi_VN",
+    "domain": "facebook.com",
+    "path": "/",
+    "secure": true,
+    "httpOnly": false
+  },
+  {
+    "key": "ps_l",
+    "value": "1",
+    "domain": "facebook.com",
+    "path": "/",
+    "secure": true,
+    "httpOnly": false
+  },
+  {
+    "key": "wl_cbv",
+    "value": "v2%3Bclient_version%3A3190%3Btimestamp%3A1781454649",
+    "domain": "facebook.com",
+    "path": "/",
+    "secure": true,
+    "httpOnly": false
+  }
+]
